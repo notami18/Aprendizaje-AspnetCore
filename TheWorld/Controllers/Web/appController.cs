6a8609ab872a32feb869748c5d10d4a56433f7e0
Andres Notami <andresnotami@gmail.com>
2017-07-06 15:34:23 -0500

    New Repository

---
+++ TheWorld/Controllers/Web/appController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheWorld.Controllers.Web
+{
+    public class appController : Controller
+    {
+        public IActionResult Index() {  
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            //throw new InvalidOperationException("LAs cosas malas solo le pasan a los desarrolladores buenos");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ TheWorld/wwwroot/js/oursite.js
@@ -0,0 +1,34 @@
+﻿// Site.js
+(function () {
+    //let ele = $("#username");
+    //ele.text("Carlos Andres");
+
+    //var main = $("#main");
+
+    //main.on("mouseenter",  function () {
+    //    main.style = "backgroundColor : #888;";
+    //});
+
+    //main.on("mouseleave", function () {
+    //    main.style = "";
+    //});
+
+    //var menuItems = $("ul.menu li a");
+
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //});
+
+    var $sidebarAndWrapper = $("#sidebar, #wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Aparece Sidebar");
+        } else {
+            $(this).text("Desaparece Sidebar");
+        }
+    });
+
+})();
