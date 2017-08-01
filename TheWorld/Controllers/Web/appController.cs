44dd57007b83e6295e3c3ed0ad1c8e5055b1af61
Andres <minocamc-18@hotmail.com>
2017-08-01 09:30:45 -0500

    Fundamentios en MVC6

--- TheWorld/Controllers/Web/appController.cs
+++ TheWorld/Controllers/Web/appController.cs
@@ -1,21 +1,48 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TheWorld.Services;
+using TheWorld.ViewModels;
 
 namespace TheWorld.Controllers.Web
 {
     public class appController : Controller
     {
+        private IMailService _mailService;
+        private IConfigurationRoot _config;
+
+        public appController(IMailService mailService, IConfigurationRoot  config) {
+            _mailService = mailService;
+            _config = config;
+        }
+
         public IActionResult Index() {  
             return View();
         }
 
         public IActionResult Contact()
         {
-            //throw new InvalidOperationException("LAs cosas malas solo le pasan a los desarrolladores buenos");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (model.Email.Contains("aol.com"))
+                ModelState.AddModelError("Email", "No apoyamos la dirección de AOL");
+
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMail(_config["MailSettings:ToAddress"], model.Email, "From: Hola TheWorld", model.Message);
+
+                ModelState.Clear();
+                ViewBag.UserMessage = "Mensaje enviado corectamente";
+            }
+            
             return View();
         }
 
---
+++ TheWorld/ViewModels/ContactViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheWorld.ViewModels
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [EmailAddress(ErrorMessage = "Por favor introduzaca una direccion de correo electrónico válida.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [StringLength(4096, MinimumLength = 10, ErrorMessage = "El campo Mensaje debe ser una cadena con una longitud mínima de 10 y una longitud máxima de 4096")]
+        public string Message { get; set; } 
+    }
+}
